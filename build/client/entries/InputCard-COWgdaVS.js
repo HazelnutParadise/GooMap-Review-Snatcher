82d3dc0c9425d5fa7dee2f3fa6d44e240f320023
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2025-02-13 02:47:13 +0800

    新增 InputCard 和 InfoCard 組件，刪除舊版組件，更新 App.svelte 標題和公告區域

---
+++ build/client/entries/InputCard-COWgdaVS.js
@@ -0,0 +1,1 @@
+import"../chunks/index-CxwuwH1G.js";import{I as p}from"../chunks/InputCard-DQwDkEWn.js";import"./InfoCard-COxViyc-.js";export{p as default};
--- src/pages/App.svelte
+++ src/pages/App.svelte
@@ -118,7 +118,7 @@
     <title>{title} - 榛果繽紛樂</title>
 </svelte:head>
 
-<div id="navbar-placeholder"></div>
+<div id="navbar-placeholder" style="height: 50px;"></div>
 <div id="Pistachio-Announcement"></div>
 <div class="container">
     <img src="https://src.hazelnut-paradise.com/GooMapReviewSnatcher.png" alt="GooMapReviewSnatcher" style="width: 100%; max-width: 150px;"/>
