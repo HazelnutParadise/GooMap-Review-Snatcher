4d4976f254887f6738774feb0bf2b38d143a49c7
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2025-02-13 02:44:55 +0800

    更新 App.svelte，調整標題和新增公告區域

--- src/pages/App.svelte
+++ src/pages/App.svelte
@@ -118,8 +118,8 @@
     <title>{title} - 榛果繽紛樂</title>
 </svelte:head>
 
-<div id="Pistachio-Announcement"></div>
 <div id="navbar-placeholder"></div>
+<div id="Pistachio-Announcement"></div>
 <div class="container">
     <img src="https://src.hazelnut-paradise.com/GooMapReviewSnatcher.png" alt="GooMapReviewSnatcher" style="width: 100%; max-width: 150px;"/>
     <div class="title-box">
