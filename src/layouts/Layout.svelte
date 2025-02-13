71676d59741d161d4102975aef4f6e9650f6eda0
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2025-02-13 18:12:12 +0800

    new file:   build/client/chunks/InputCard-CDhMZJta.js 	deleted:    build/client/chunks/InputCard-n9giYpCt.js 	renamed:    build/client/chunks/index-CJP0XVN7.js -> build/client/chunks/index-uaXyWQYT.js 	new file:   build/client/entries/App-DMwFUcvc.js 	deleted:    build/client/entries/App-xTnD1xMK.js 	renamed:    build/client/entries/InfoCard-BRJ-pcms.js -> build/client/entries/InfoCard-Bv-KHVvf.js 	new file:   build/client/entries/InputCard-BeEPT1Ij.js 	deleted:    build/client/entries/InputCard-Bgllh157.js 	deleted:    build/client/entries/Layout-BCl4RSDh.js 	new file:   build/client/entries/Layout-CPhLEwW_.js 	renamed:    build/client/entries/Loader-DaCgnKIg.js -> build/client/entries/Loader-CDWGFKNR.js 	renamed:    build/client/entries/default-error-BCHEcw37.js -> build/client/entries/default-error-DJh6UE25.js 	renamed:    build/client/entries/hydrate-C8Jkq4K-.js -> build/client/entries/hydrate-Bf51cNiX.js 	modified:   build/server/render.js 	modified:   src/layouts/Layout.svelte

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -1,5 +1,4 @@
 <div id="navbar-placeholder" style="min-height: 50px; z-index: 100;"></div>
-<div id="Pistachio-Announcement" bind:this={bannerBox}></div>
 <slot></slot>
 <footer>
     2025 - 榛果繽紛樂
@@ -16,35 +15,3 @@
         align-items: center;
     }
 </style>
-
-<script>
-    import { onMount } from 'svelte';
-    let bannerBox;
-    let announcement;
-    onMount(() => {
-        if (bannerBox.innerHTML !== '') announcement = bannerBox.innerHTML;
-
-        // 使用 MutationObserver 監聽 bannerBox 內容變化
-        const observer = new MutationObserver(() => {
-            const closeButton = bannerBox.querySelector("Pistachio-Announcement＿global-banner").querySelector('#close-banner');
-            if (closeButton) {
-                closeButton.addEventListener('click', () => {
-                    bannerBox.style.display = 'none';
-                });
-            }
-        });
-
-        observer.observe(bannerBox, { childList: true, subtree: true });
-    });
-
-    $: { 
-        if (announcement) {
-            if (bannerBox.innerHTML !== ''){
-                announcement = bannerBox.innerHTML;
-            }else {
-                bannerBox.innerHTML = announcement;
-                bannerBox.style.display = 'block';
-            }
-        }
-    }
-</script>
