37b86395ccf143d92ce2bf75e59f4fbcd3376619
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2025-02-13 17:04:40 +0800

    新增 Layout 組件，更新公告橫幅顯示邏輯，移除不必要的檔案，並調整樣式以改善可讀性

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -1,5 +1,5 @@
 <div id="navbar-placeholder" style="min-height: 50px; z-index: 100;"></div>
-<div id="Pistachio-Announcement" style="min-height: 50px;"></div>
+<div id="Pistachio-Announcement" bind:this={bannerBox}>公告橫幅</div>
 <slot></slot>
 <footer>
     2025 - 榛果繽紛樂
@@ -15,8 +15,25 @@
         justify-content: center;
         align-items: center;
     }
-
-    #Pistachio-Announcement * {
-        z-index: 100;
-    }
 </style>
+
+<script>
+    import { onMount } from 'svelte';
+    let bannerBox;
+    let announcement;
+    onMount(()=>{
+        bannerBox = document.getElementById('Pistachio-Announcement');
+        if (bannerBox.innerHTML.trim() === '公告橫幅') {
+            bannerBox.style.display = 'none';
+        } else {
+            bannerBox.style.display = 'block';
+            announcement = bannerBox.innerHTML;
+        }
+    });
+
+    $: {
+        if (announcement) {
+            bannerBox.innerHTML = announcement;
+        }
+    }
+</script>
