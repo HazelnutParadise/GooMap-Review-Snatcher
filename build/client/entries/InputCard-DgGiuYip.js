1532900cd2d95ec753b0c96aba4d159fdccac9bc
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2025-03-07 09:53:13 +0800

    新增 each-D6YF6ztN.js，刪除 InputCard-DgGiuYip.js 和 InfoCard-VSrB1MFF.js，新增 default-error-BG9NszCF.js、Loader-CT3sUNCL.js、OneAd-DLrgNz36.js、default-error-B19OOPLr.js、Loader-Cd-iapTC.js、OneAd-CEBZBcLO.js 和 Layout-D4dfSEvq.js，並更新 App-C5gkbtwu.css 的樣式。

--- build/client/entries/InputCard-DgGiuYip.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/index-CtmGPglQ.js";import{I as p}from"../chunks/InputCard-BQDcxm56.js";import"./InfoCard-VSrB1MFF.js";export{p as default};
--- src/pages/App.svelte
+++ src/pages/App.svelte
@@ -2,6 +2,7 @@
   import { onMount } from "svelte";
   import InputCard from "../components/InputCard.svelte";
   import Loader from "../components/Loader.svelte";
+  import Result from "../components/Result.svelte";
   export let title;
   export let subtitle;
   const searchStoreUrl = "/api/search";
@@ -150,73 +151,7 @@
       {storeData}
     />
   {:else}
-    <button
-      class="reset-button button"
-      on:click={() => {
-        searchInputStr = "";
-        storeData = [];
-        reviews = [];
-      }}>查詢其他商家</button
-    >
-    <div class="review-box">
-      <h3 class="store-name">{selectedStore.Name}</h3>
-      <button class="download-button button" on:click={() => download("csv")}
-        >下載 CSV</button
-      >
-      <button class="download-button button" on:click={() => download("json")}
-        >下載 JSON</button
-      >
-      <button
-        class="mining-button button"
-        on:click={async () => {
-          const reviewContent = reviews.map((review) => review.content);
-          let dataUUID = "";
-
-          const res = await fetch("/api/review-mining", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              storeName: selectedStore.Name,
-              reviews: reviewContent,
-            }),
-          });
-          if (res.ok) {
-            const data = await res.json();
-            dataUUID = data.dataUUID;
-          } else {
-            console.error("Error:", res.statusText);
-            alert("無法使用 StoreCoach 探勘");
-            return;
-          }
-          const a = document.createElement("a");
-          a.href = `https://storecoach.hazelnut-paradise.com/review-mining/${dataUUID}`;
-          a.target = "_blank";
-          a.click();
-        }}>使用 StoreCoach 探勘</button
-      >
-      <div class="review-table-box">
-        <table class="review-table">
-          <thead>
-            <tr>
-              {#each Object.keys(reviews[0]) as key}
-                <th>{key}</th>
-              {/each}
-            </tr>
-          </thead>
-          <tbody>
-            {#each reviews as review}
-              <tr>
-                {#each Object.values(review) as value}
-                  <td>{value}</td>
-                {/each}
-              </tr>
-            {/each}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <Result {selectedStore} {searchInputStr} {storeData} {reviews} {download} />
   {/if}
 </div>
 
@@ -256,123 +191,6 @@
     font-size: 1.5rem;
     color: #0a0903;
     margin: 0;
-  }
-
-  .review-box {
-    width: 80%;
-  }
-
-  .store-name {
-    font-size: 1.5rem;
-  }
-
-  .review-table {
-    border-collapse: collapse;
-    width: 100%;
-    background-color: white;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    border-radius: 8px;
-    overflow: hidden;
-  }
-
-  .review-table th {
-    background-color: #2c3e50;
-    color: white;
-    padding: 12px 15px;
-    text-align: left;
-    font-weight: 600;
-  }
-
-  .review-table td {
-    padding: 12px 15px;
-    border-bottom: 1px solid #ddd;
-  }
-
-  .review-table tbody tr:hover {
-    background-color: #f5f5f5;
-    transition: background-color 0.2s ease;
-  }
-
-  .review-table tbody tr:last-child td {
-    border-bottom: none;
-  }
-
-  .review-table-box {
-    width: 100%;
-    height: 500px;
-    overflow: auto;
-    background-color: white;
-    border-radius: 8px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-    padding: 1rem;
-  }
-
-  /* 美化捲軸 */
-  .review-table-box::-webkit-scrollbar {
-    width: 8px;
-    height: 8px;
-  }
-
-  .review-table-box::-webkit-scrollbar-track {
-    background: #f1f1f1;
-    border-radius: 4px;
-  }
-
-  .review-table-box::-webkit-scrollbar-thumb {
-    background: #888;
-    border-radius: 4px;
-  }
-
-  .review-table-box::-webkit-scrollbar-thumb:hover {
-    background: #555;
-  }
-
-  .reset-button {
-    height: 50px;
-    width: 20%;
-    min-width: 200px;
-    border-radius: 15px;
-    border: none;
-    background-color: #0a0903;
-    color: white;
-    font-size: 1rem;
-    cursor: pointer;
-    margin-bottom: 1rem;
-  }
-
-  .download-button {
-    height: 40px;
-    width: 20%;
-    min-width: 200px;
-    border-radius: 15px;
-    border: none;
-    background-color: #0a0903;
-    color: white;
-    font-size: 1rem;
-    cursor: pointer;
-    margin-bottom: 1rem;
-  }
-
-  .button:hover {
-    background-color: #f1f1f1;
-    color: #0a0903;
-  }
-
-  .mining-button {
-    height: 40px;
-    width: 20%;
-    min-width: 200px;
-    border-radius: 15px;
-    border: none;
-    font-size: 1rem;
-    cursor: pointer;
-    margin-bottom: 1rem;
-    background-color: #ffa400;
-    color: black;
-  }
-
-  .mining-button:hover {
-    background-color: rgb(255, 128, 0);
   }
 
   @keyframes pop {
